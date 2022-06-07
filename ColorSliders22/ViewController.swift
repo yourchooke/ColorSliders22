9706585d290d15f1dcd06f42fc01efa7567718ee
Olga Yurchuk <olga.yurchuk@xored.com>
2022-06-07 18:37:17 +0700

    Smooth view

--- ColorSliders22/ViewController.swift
+++ ColorSliders22/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coloredView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        coloredView.layer.cornerRadius = 20
     }
 
 
