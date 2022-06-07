90f9d37a93b93fa4b4ce962e3b7b8163e02f62eb
Olga Yurchuk <olga.yurchuk@xored.com>
2022-06-07 19:08:40 +0700

    Initial conditions of main view.

--- ColorSliders22/ViewController.swift
+++ ColorSliders22/ViewController.swift
@@ -11,11 +11,37 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var coloredView: UIView!
     
+    @IBOutlet weak var redSliderPicker: UISlider!
+    @IBOutlet weak var greenSliderPicker: UISlider!
+    @IBOutlet weak var blueSliderPicker: UISlider!
+    
+    @IBOutlet weak var redColorValueLabel: UILabel!
+    @IBOutlet weak var greenColorValueLabel: UILabel!
+    @IBOutlet weak var blueColorValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         coloredView.layer.cornerRadius = 20
+        
+        redSliderPicker.value = 0
+        greenSliderPicker.value = 0
+        blueSliderPicker.value = 0
+        
+        redColorValueLabel.text = String(redSliderPicker.value)
+        greenColorValueLabel.text = String(greenSliderPicker.value)
+        blueColorValueLabel.text = String(blueSliderPicker.value)
+        
     }
 
-
+    @IBAction func redSliderMove() {
+    }
+    
+    @IBAction func greenSliderMove() {
+    }
+    
+    @IBAction func blueSliderMove() {
+    }
+    
+    
 }
 
